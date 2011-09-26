a2bae71778a4e8410d5444a1dcf673144bd3cbb1
cloud <cloudcry@gmail.com>
2011-09-26 15:32:58 +0800

    fuck this world

--- utils/sp.py
+++ utils/sp.py
@@ -33,8 +33,8 @@ def sms_send(phone, msg, msg_type):
     uri = u"http://utf8.sms.webchinese.cn/?Uid=cloud&Key=q12wer43ui8765tyop09&smsMob=%(phone)s&smsText=%(msg)s"
     uri = uri % {'phone': phone, 'msg': tornado.escape.url_escape(msg_content.encode('utf-8'))}
 
-    http_client = httpclient.AsyncHTTPClient()
-    http_client.fetch(uri, handle_request)
+    http_client = tornado.httpclient.AsyncHTTPClient()
+    http_client.fetch(uri, handler_sms_reqeust)
 
     tornado.ioloop.IOLoop.instance().start()
 
