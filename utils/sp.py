336d6bc91056fbec0dde42d89954acc27d37101e
cloud <cloudcry@gmail.com>
2011-09-26 15:42:03 +0800

    fix bug

--- utils/sp.py
+++ utils/sp.py
@@ -14,6 +14,20 @@ errors = {
         -42: u"短信内容为空",
         }
 
+def get_sms_template(msg_type):
+    res = u""
+    if msg_type == 1:
+        res = u"乐帮注册验证码:%(code)s。乐帮感谢你的注册！"
+    elif msg_type == 2:
+        res = u"%(name)s，你好！重置后的密码:%(password)s。乐帮提醒你尽快修改！"
+    return res
+
+def ret_code2desc(ret_code):
+    if ret_code < 0:
+        return errors[ret_code]
+    else:
+        return u"成功发送%d条" % ret_code
+
 def handler_sms_reqeust(response):
     if response.error:
         print "Oops! error to request the sms service."
@@ -31,23 +45,9 @@ def sms_send(phone, msg, msg_type):
 
     # sms api uri
     uri = u"http://utf8.sms.webchinese.cn/?Uid=cloud&Key=q12wer43ui8765tyop09&smsMob=%(phone)s&smsText=%(msg)s"
-    uri = uri % {'phone': phone, 'msg': escape.url_escape(escape.utf8(msg_content.encode))}
+    uri = uri % {'phone': phone, 'msg': escape.url_escape(escape.utf8(msg_content))}
 
     http_client = httpclient.AsyncHTTPClient()
     http_client.fetch(uri, handler_sms_reqeust)
 
     ioloop.IOLoop.instance().start()
-
-def get_sms_template(msg_type):
-    res = u""
-    if msg_type == 1:
-        res = u"乐帮注册验证码:%(code)s。乐帮感谢你的注册！"
-    elif msg_type == 2:
-        res = u"%(name)s，你好！重置后的密码:%(password)s。乐帮提醒你尽快修改！"
-    return res
-
-def ret_code2desc(ret_code):
-    if ret_code < 0:
-        return errors[ret_code]
-    else:
-        return u"成功发送%d条" % ret_code
