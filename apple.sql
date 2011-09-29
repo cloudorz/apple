dbc9187b05fc5ac8fcda5b1fd246a899a9ab51ec
cloud <cloudcry@gmail.com>
2011-09-30 00:29:30 +0800

    fix bug

--- apple.sql
+++ apple.sql
@@ -1,38 +1,41 @@
---   mysql --user=blog --password=blog --database=blog < apple.sql
+--   mysql --user=apple --password=apple --database=apple < apple.sql
 SET SESSION storage_engine = "InnoDB";
 SET SESSION time_zone = "+8:00";
 ALTER DATABASE CHARACTER SET "utf8";
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    phone BIGINT UNSIGNED NOT NULL UNIQUE,
-    password VARCHAR(32) NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    avatar VARCHAR(100) NULL,
-    token VARCHAR(32) NULL,
-    last_lat DOUBLE NOT NULL DEFAULT 0,
-    last_lon DOUBLE NOT NULL DEFAULT 0,
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    phone bigint(20) unsigned NOT NULL UNIQUE,
+    password varchar(32) NOT NULL,
+    name varchar(20) NOT NULL,
+    avatar varchar(100) NULL,
+    token varchar(32) NULL,
+    last_lat double NOT NULL DEFAULT '0',
+    last_lon double NOT NULL DEFAULT '0',
     /*loud_num INT NOT NULL DEFAULT 0,*/
-    radius FLOAT NULL,
-    shadow VARCHAR(2048) NULL,
-    is_admin TINYINT(1) NOT NULL DEFAULT 0,
-    block TINYINT(1) NOT NULL DEFAULT 0,
-    updated DATETIME NOT NULL,
-    created DATETIME NOT NULL
+    radius float NULL,
+    shadow varchar(2048) NULL,
+    is_admin tinyint(1) NOT NULL DEFAULT '0',
+    block tinyint(1) NOT NULL DEFAULT '0',
+    updated datetime NOT NULL,
+    created datetime NOT NULL,
+    PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS louds;
 CREATE TABLE louds (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNSIGNED NOT NULL REFERENCES users(id),
-    content VARCHAR(70) NOT NULL,
-    address VARCHAR(30) NULL,
-    lat DOUBLE NOT NULL,
-    lon DOUBLE NOT NULL,
-    grade SMALLINT(2) NOT NULL DEFAULT 5,
-    block TINYINT(1) NOT NULL DEFAULT 0,
-    created DATETIME NOT NULL
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    user_id int(10) unsigned NOT NULL,
+    content varchar(70) NOT NULL,
+    address varchar(30) NULL,
+    lat double NOT NULL,
+    lon double NOT NULL,
+    grade smallint(2) NOT NULL DEFAULT '5',
+    block tinyint(1) NOT NULL DEFAULT '0',
+    created datetime NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 /*
