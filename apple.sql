02ab9c269020c11325a2156bde5e077d52d09a16
cloud <cloudcry@gmail.com>
2011-10-01 15:36:00 +0800

    new sql

--- apple.sql
+++ apple.sql
@@ -1,58 +1,78 @@
---   mysql --user=apple --password=apple --database=apple < apple.sql
-SET SESSION storage_engine = "InnoDB";
-SET SESSION time_zone = "+8:00";
-ALTER DATABASE CHARACTER SET "utf8";
+-- MySQL dump 10.13  Distrib 5.1.56, for pc-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: apple
+-- ------------------------------------------------------
+-- Server version	5.1.56-log
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id int(10) unsigned NOT NULL AUTO_INCREMENT,
-    phone bigint(20) unsigned NOT NULL UNIQUE,
-    password varchar(32) NOT NULL,
-    name varchar(20) NOT NULL,
-    avatar varchar(100) NULL,
-    token varchar(32) NULL,
-    last_lat double NOT NULL DEFAULT '0',
-    last_lon double NOT NULL DEFAULT '0',
-    /*loud_num INT NOT NULL DEFAULT 0,*/
-    radius float NULL,
-    shadow varchar(2048) NULL,
-    is_admin tinyint(1) NOT NULL DEFAULT '0',
-    block tinyint(1) NOT NULL DEFAULT '0',
-    updated datetime NOT NULL,
-    created datetime NOT NULL,
-    PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP TABLE IF EXISTS louds;
-CREATE TABLE louds (
-    id int(10) unsigned NOT NULL AUTO_INCREMENT,
-    user_id int(10) unsigned NOT NULL,
-    content varchar(70) NOT NULL,
-    address varchar(30) NULL,
-    lat double NOT NULL,
-    lon double NOT NULL,
-    grade smallint(2) NOT NULL DEFAULT '5',
-    block tinyint(1) NOT NULL DEFAULT '0',
-    created datetime NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+--
+-- Table structure for table `louds`
+--
 
-/*
-DROP TABLE IF EXISTS apps;
-CREATE TABLE APPS (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(10) NOT NULL UNIQUE,
-    key VARCHAR(11) NOT NULL,
-    secret VARCHAR(32) NOT NULL,
-    created DATETIME
-);
+DROP TABLE IF EXISTS `louds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `louds` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `content` varchar(70) NOT NULL,
+  `address` varchar(30) DEFAULT NULL,
+  `lon` double NOT NULL,
+  `lat` double NOT NULL,
+  `grade` smallint(2) NOT NULL DEFAULT '5',
+  `block` tinyint(1) NOT NULL DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `louds_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS vars;
-CREATE TABLE vars (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(10) NOT NULL UNIQUE,
-    value FLOAT NOT NULL
-);
+--
+-- Table structure for table `users`
+--
 
-*/
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `phone` bigint(20) unsigned NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `avatar` varchar(100) DEFAULT NULL,
+  `token` varchar(32) DEFAULT NULL,
+  `last_lon` double NOT NULL DEFAULT '0',
+  `last_lat` double NOT NULL DEFAULT '0',
+  `radius` float DEFAULT NULL,
+  `shadow` varchar(2048) DEFAULT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `block` tinyint(1) NOT NULL DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-10-01 15:33:37
