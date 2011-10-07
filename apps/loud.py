8d1348ff54351aa84b84b5d8639ef0d81b12a12f
cloud <cloudcry@gmail.com>
2011-10-07 17:10:47 +0800

    gu kuai

--- apps/loud.py
+++ apps/loud.py
@@ -27,7 +27,7 @@ class LoudHandler(BaseRequestHandler):
     def post(self, lid):
 
         # the precondtion the max 3 louds.
-        loud_count = Loud.query.get_louds().filter(Loud.user==self.current_user)).count()
+        loud_count = Loud.query.get_louds().filter(Loud.user==self.current_user).count()
         if loud_count > 3:
             raise HTTPError(412)
 
